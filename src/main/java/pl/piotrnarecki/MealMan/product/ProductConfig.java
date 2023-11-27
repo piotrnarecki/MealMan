3b2f87f0be6f773d637cc990a69f1bc0d78e7eac
Piotr Narecki <nareckipiotraleksander@gmail.com>
2023-11-27 19:12:46 +0100

    development branch init

--- src/main/java/pl/piotrnarecki/MealMan/product/ProductConfig.java
+++ src/main/java/pl/piotrnarecki/MealMan/product/ProductConfig.java
@@ -14,17 +14,11 @@ public class ProductConfig {
     @Bean
     CommandLineRunner productCommandLineRunner(ProductRepository productRepository){
 
-
-
         return args -> {
-
 
             Product product1 = new Product("egg","protein",100);
             Product product2 = new Product("bacon","protein",300);
             Product product3 = new Product("steak","protein",500);
-
-
-
 
             productRepository.saveAll(List.of(product1,product2,product3));
 
